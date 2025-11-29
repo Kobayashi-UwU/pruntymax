477ec157a55ce838475f78ed12c1de6809cc8910
pkhongsawatk <panumeth.khongsawatk@agoda.com>
2025-11-29 14:44:49 +0700

    fix - vercel deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,8 +23,8 @@ const config = {
       },
       keyframes: {
         fadeUp: {
-          "0%": { opacity: 0, transform: "translateY(8px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
+          "0%": { opacity: "0", transform: "translateY(8px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
       },
       animation: {
